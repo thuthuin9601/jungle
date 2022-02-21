4772b7233a5b6ccc95c9ea1b08e27e28e4521887
thuthuin9601 <96036874+thuthuin9601@users.noreply.github.com>
2022-02-21 07:48:11 +0700

    done

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 768;
 const CANVAS_HEIGHT = canvas.height = 576;
-let gamespeed = 7;
+let gamespeed = 2;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'far-buildings.png';
 const backgroundLayer2 = new Image();
-backgroundLayer2.src = 'back-building.png';
+backgroundLayer2.src = 'back-buildings.png';
 const backgroundLayer3 = new Image();
-backgroundLayer2.src = 'foreground.png';
+backgroundLayer3.src = 'foreground.png';
 
 const slider = document.getElementById('slider');
 slider.value = gamespeed;
@@ -21,10 +21,10 @@ slider.addEventListener('change', function(e){
 });
 
 class Layer{
-    constructor(image, speedModifier){
+    constructor(image, speedModifier, width){
         this.x = 0;
         this.y = 0;
-        this.width = 768;
+        this.width = width;
         this.height = 576;
         this.x2 = this.width;
         this.image = image;
@@ -47,9 +47,9 @@ class Layer{
         ctx.drawImage(this.image, this.x2, this.y, this.width, this.height);
     }
 };
-const layer1 = new Layer(backgroundLayer1, 2.5);
-const layer2 = new Layer(backgroundLayer2, 2.5);
-const layer3 = new Layer(backgroundLayer3, 2.5);
+const layer1 = new Layer(backgroundLayer1, 1.5, 768);
+const layer2 = new Layer(backgroundLayer2, 2, 768);
+const layer3 = new Layer(backgroundLayer3, 2.5, 1056);
 
 const gameObjects = [layer1, layer2, layer3];
 
